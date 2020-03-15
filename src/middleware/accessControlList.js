ced2d6c09a1f9437b03e06af01875764a9828d94
‘peter-robert-cole’ <petercole3@gmail.com>
2020-03-15 16:29:47 -0700

    closer to full boiler plate

--- src/middleware/accessControlList.js
+++ src/middleware/accessControlList.js
@@ -6,17 +6,22 @@ function accessControlList() {
       if (req.user.role.permissions.includes(permissions)) {
         next()
       } else {
+        // NOTES - on what next() does
         // when you call next with anything other than a 
         // request object as the first parameter, Express
-        // treats it as an error and hsuntes it to whihcever middleware function handles the error first nby having e.g. function errorHanderler(rerr, res, req, nxt) -- 
-        // so, we could call this as next('you shall not pass') or next(new Error('you shall not pass')) and have it properly handled by the errorHandling middleware we wrote which expects an error object
+        // treats it as an error and hunts it to whichever 
+        // middleware function handles the error first by 
+        // having function errorHanderler(rerr, res, req, nxt) -- 
+        // so, we could call this as next('you shall not pass') 
+        // or next(new Error('you shall not pass')) and have it 
+        // properly handled by the errorHandling middleware 
+        // we wrote which expects an error object
         next('You shall not Pass')
       }
     } catch { error } {
       next(error);
     }
-  }
-  
+  } 
 }
 
 module.exports = accessControlList;
